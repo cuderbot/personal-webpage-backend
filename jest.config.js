19c827a6fe9c198e1c81073aa489514563d01201
Javier <ja.becerraa@gmail.com>
2021-02-12 00:06:28 -0300

    chore(husky,linstaged,jest): moving into separate files

---
+++ jest.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  moduleFileExtensions: ['js'],
+  rootDir: 'test/unit',
+  testRegex: '.*\\.(spec|test)\\.js$',
+  collectCoverageFrom: ['**/*.js'],
+  coverageDirectory: '../coverage',
+  testEnvironment: 'node',
+};
