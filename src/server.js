f60e48150e90d150e64cc1903804a6fb0a3a97eb
Javier <ja.becerraa@gmail.com>
2021-02-12 17:16:54 -0300

    fix(server): removing unused dependency

--- src/server.js
+++ src/server.js
@@ -2,7 +2,6 @@ const express = require('express');
 const morgan = require('morgan');
 const helmet = require('helmet');
 const cors = require('cors');
-const pathfinderUI = require('pathfinder-ui');
 
 require('dotenv/config');
 
