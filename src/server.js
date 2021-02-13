191b26bf22ca04f7a7d10fd724233a4553ee6c5e
Javier <ja.becerraa@gmail.com>
2021-02-13 15:24:42 -0300

    feat: adding app/form-encoded

--- src/server.js
+++ src/server.js
@@ -15,6 +15,9 @@ module.exports = app;
 app.use(morgan('dev'));
 app.use(helmet());
 app.use(cors());
+// parse requests of content-type - application/x-www-form-urlencoded
+app.use(express.urlencoded({ extended: true }));
+// parse requests of content-type - application/json
 app.use(express.json());
 
 app.use('/api', router);
