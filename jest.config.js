f6ad21e4c70b93203c25b16502e7755a347c8748
Javier <ja.becerraa@gmail.com>
2021-02-22 16:38:35 -0300

    chore: moving config files into package.json

--- jest.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  moduleFileExtensions: ['js'],
-  rootDir: 'test/unit',
-  testRegex: '.*\\.(spec|test)\\.js$',
-  collectCoverageFrom: ['**/*.js'],
-  coverageDirectory: '../coverage',
-  testEnvironment: 'node',
-};
